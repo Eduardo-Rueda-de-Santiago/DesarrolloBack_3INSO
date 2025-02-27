9e0b5893723ea26fd65c109f7f668429b8947eed
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-02-27 11:15:02 +0100

    Added logical delete for mongoose

--- ProyectoDeCurso/models/nosql/tracks.ts
+++ ProyectoDeCurso/models/nosql/tracks.ts
@@ -1,52 +1,53 @@
 import { Schema, model, Types } from "mongoose";
+import mongooseDelete from "mongoose-delete";
 
 const TracksScheme = new Schema(
-    {
-        name: {
-            type: String
-        },
-        album: {
-            type: String
-        },
-        cover: {
-            type: String,
-            validate: {
-                validator: (req) => {
-                    return true; //TODO crear patrón
-                },
-                message: "ERROR_URL",
-            }
-        },
-        artist: {
-            name: {
-                type: String,
-            },
-            nickname: {
-                type: String
-            },
-            nationality: {
-                type: String
-            }
-        },
-        duration: {
-            start: {
-                type: Number
-            },
-            end: {
-                type: Number
-            }
-        },
-        mediaId: {
-            type: Types.ObjectId // Estructura (string) especial de mongo
-        }
-    },
-    {
-        timestamp: true, // TODO createdAt, updatedAt
-        versionKey: false
-    }
+	{
+		name: {
+			type: String
+		},
+		album: {
+			type: String
+		},
+		cover: {
+			type: String,
+			validate: {
+				validator: (req) => {
+					return true; //TODO crear patrón
+				},
+				message: "ERROR_URL",
+			}
+		},
+		artist: {
+			name: {
+				type: String,
+			},
+			nickname: {
+				type: String
+			},
+			nationality: {
+				type: String
+			}
+		},
+		duration: {
+			start: {
+				type: Number
+			},
+			end: {
+				type: Number
+			}
+		},
+		mediaId: {
+			type: Types.ObjectId // Estructura (string) especial de mongo
+		}
+	},
+	{
+		timestamp: true, // TODO createdAt, updatedAt
+		versionKey: false
+	}
 )
 const TracksModel = model('Track', TracksScheme);
 
-export default TracksModel;
+TracksScheme.plugin(mongooseDelete, { overrideMethods: "all" })
 
-// TracksScheme.plugin(mongooseDelete, {overrideMethods: "all"})
+export default TracksModel;
