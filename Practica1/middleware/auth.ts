30caddd337a92bcc550a2748e1e7c347bc75d364
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-03-11 11:45:46 +0100

    Added user files

---
+++ Practica1/middleware/auth.ts
@@ -0,0 +1,45 @@
+import JsonWebTokenService from "../services/jsonWebToken";
+import UserService from "../services/user";
+
+/**
+ * Checks if the token in the request exists and is valid in the database.
+ * Also adds a user field to the query with the object of the user who launched the query.
+ * 
+ * @param req Request
+ * @param res Response
+ * @param next Next field
+ */
+export default async function auth(req: any, res: any, next: any) {
+
+	try {
+
+		const token = req.header('Authorization').split(" ").pop();
+
+		if (!token){
+			throw new Error("No hay token!");
+		}
+
+		const jsonWebTokenService: JsonWebTokenService = new JsonWebTokenService();
+
+		const decoded = jsonWebTokenService.verifyToken(token);
+
+		const userService: UserService = new UserService();
+
+		const user = await userService.getUserById(decoded.userId);
+
+		if (!user) {
+			return res.status(401).json({ error: 'The user doesn\'t exist!' });
+		}
+
+		req.user = user;
+
+		next();
+
+	} catch (error) {
+
+		console.error(error);
+		return res.status(401).json({ error: error.message });
+
+	}
+
+}
