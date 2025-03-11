d035a791661cf42cf53baa94ed686a12548ea201
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-03-11 12:33:03 +0100

    Added validator

--- Practica1/routers/user.ts
+++ Practica1/routers/user.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import * as controller from "../controllers/user";
+import * as validator from "../validators/user";
 
 const userRouter: Router = Router();
 
 userRouter.post(
 	"/register",
+	validator.registerUser,
 	controller.registerUser
 );
 
---
+++ Practica1/validators/user.ts
@@ -0,0 +1,16 @@
+import { body } from "express-validator";
+import validateResults from "../services/validator";
+
+export const registerUser = [
+	
+	body("email")
+		.isEmail().withMessage("Se debe introducir un email")
+		.isLength({ max: 100 }).withMessage("El tamaño máximo del email son 100 caracteres."),
+
+	body("password")
+		.isString()
+		.isStrongPassword({ minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 }).withMessage("La contraseña debe tener al menos una minúscula, una mayúscula y un número.")
+		.isLength({ min: 8, max: 50 }).withMessage("La contraseña debe tener entre 8 y 50 caracteres."),
+
+	(req: any, res: any, next: any) => validateResults(req, res, next)
+];
