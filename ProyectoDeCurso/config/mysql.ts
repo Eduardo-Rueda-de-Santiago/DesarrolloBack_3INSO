a7b09b4f7a1e6170362660150f73bfa1fec49314
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2025-03-20 12:32:33 +0100

    a

--- ProyectoDeCurso/config/mysql.ts
+++ ProyectoDeCurso/config/mysql.ts
@@ -1,14 +1,13 @@
 import { Sequelize } from "sequelize";
 
-// export const sequelize = new Sequelize({
-// 	// database: process.env.MYSQL_DATABASE,
-// 	username: "root",
-// 	password: "password",
-// 	host: "10.1.205.95",
-// 	port: 3306,
-// 	dialect: "mysql",
-// });
-export const sequelize = new Sequelize("mysql://um8ewsw2zts1oij0:dYUmVULurjbuY5p0m1RK@bnobmuqt1igaprlxstnm-mysql.services.clever-cloud.com:3306/bnobmuqt1igaprlxstnm");
+export const sequelize = new Sequelize({
+	// database: process.env.MYSQL_DATABASE,
+	username: "root",
+	password: "password",
+	host: "10.1.205.95",
+	port: 3306,
+	dialect: "mysql",
+});
 export const dbConnectMySql = async () => {
 	try {
 		await sequelize.authenticate()
